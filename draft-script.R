0c6f191ae2c13248195cb4e73ff9faca581e4ea7
SpoicyCurri <spoicycurri33@gmail.com>
2021-04-19 19:58:19 +0100

    init commit

--- draft-script.R
+++ draft-script.R
@@ -1,38 +1,48 @@
 library(tidyverse)
 
-PL_raw_data <- read_csv("sportsref_download_PL_210419.csv")
-
-glimpse(PL_raw_data)
+PL_raw_data <- read_csv("data/sportsref_download_PL_210419.csv")
+LaLiga_raw_data <- read_csv("data/sportsref_download_LaLiga_210419.csv")
+SerieA_raw_data <- read_csv("data/sportsref_download_SerieA_210419.csv")
 
 # Matches that haven't been played are filtered out
 
 Big6 <- c("Arsenal", "Chelsea", "Liverpool", "Manchester City", "Manchester Utd", "Tottenham")
+Spa3 <- c("Real Madrid", "Barcelona", "Atlético Madrid")
+Ser3 <- c("Juventus", "Milan", "Inter")
+Super12 <- c(Big6, Spa3, Ser3)
 
-PL_tidy <- PL_raw_data %>%
-  filter(!is.na(Home)) %>%
-  select(Home, Away, Score) %>%
-  filter(!is.na(Score)) %>%
-  mutate("Home_Score" = str_extract(Score, "^[:digit:]"),
-         "Away_Score" = str_extract(Score, "[:digit:]$"),
-         "Home_Points" = if_else(Home_Score > Away_Score, 3,
-                                 if_else(Home_Score < Away_Score, 0, 1)),
-         "Away_Points" = if_else(Home_Score < Away_Score, 3,
-                                 if_else(Home_Score > Away_Score, 0, 1)),
-         "Big6_ind" = if_else(Home %in% Big6 & Away %in% Big6, "B", 
-                              if_else(Home %in% Big6, "H", 
-                                      if_else(Away %in% Big6, "A", "N"))),
-         "O14_Home_Points" = if_else())
+Tidy_Data <- function(League_Data){
+  League_Data %>%
+    filter(!is.na(Home)) %>%
+    select(Home, Away, Score) %>%
+    filter(!is.na(Score)) %>%
+    mutate("Home_Score" = str_extract(Score, "^[:digit:]"),
+           "Away_Score" = str_extract(Score, "[:digit:]$"),
+           "Home_Points" = if_else(Home_Score > Away_Score, 3,
+                                   if_else(Home_Score < Away_Score, 0, 1)),
+           "Away_Points" = if_else(Home_Score < Away_Score, 3,
+                                   if_else(Home_Score > Away_Score, 0, 1)),
+           "Super12_ind" = if_else(Home %in% Super12 & Away %in% Super12, "B", 
+                                   if_else(Home %in% Super12, "H", 
+                                           if_else(Away %in% Super12, "A", "N"))),
+           "O14_Home_Points" = if_else(Super12_ind %in% c("H","B"), 0, 
+                                       if_else(Super12_ind == "A", 3, Home_Points)),
+           "O14_Away_Points" = if_else(Super12_ind %in% c("A","B"), 0, 
+                                       if_else(Super12_ind == "H", 3, Away_Points)))
+}
 
-glimpse(PL_tidy)
-
-Current_Table <- PL_tidy %>%
-  select("Team" = Home, "Points" = Home_Points) %>%
-  rbind(select(PL_tidy, "Team" = Away, "Points" = Away_Points)) %>%
+Updated_League_Table <- function(League_Data){
+  League_Data %>%
+  select("Team" = Home, "Points" = Home_Points, "O14_Points" = O14_Home_Points) %>%
+  rbind(select(League_Data, "Team" = Away, "Points" = Away_Points, "O14_Points" = O14_Away_Points)) %>%
   group_by(Team) %>%
   summarise("Games" = n(),
-            "Points" = sum(Points)) %>%
-  arrange(desc(Points))
+            "Points" = sum(Points),
+            "O14_Points" = sum(O14_Points),
+            "Points_Gain" = O14_Points - Points) %>%
+  arrange(desc(O14_Points))
+}
 
-Other14_Table <- 
-
-
+SerieA_raw_data %>%
+  Tidy_Data() %>%
+  Updated_League_Table()
